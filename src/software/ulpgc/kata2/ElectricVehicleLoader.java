d3d137d2813fa368126f339cdbe1810e90b6b529
Marcos Miguel Sánchez Antonio <140960974+0xM4rc@users.noreply.github.com>
2023-12-02 20:59:45 +0000

    Delete src/software/ulpgc/kata2 directory

--- src/software/ulpgc/kata2/ElectricVehicleLoader.java
+++
@@ -1,6 +0,0 @@
-package src.software.ulpgc.kata2;
-import java.util.List;
-
-public interface ElectricVehicleLoader {
-    List<ElectricVehicle> load();
-}
--- src/software/ulpgc/kata2/ElectricVehicleStatistic.java
+++
@@ -1,8 +0,0 @@
-package src.software.ulpgc.kata2;
-
-import java.util.List;
-import java.util.Map;
-
-public interface ElectricVehicleStatistic {
-    Map<String,Integer> calculate(List<ElectricVehicle> vehicles);
-}
